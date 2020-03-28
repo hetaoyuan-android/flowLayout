8902debcf4a255fa41584fff60405a72a4f0d51e
yht110 <183023376@qq.com>
2020-03-28 12:02:19 +0800

    Initial commit

---
+++ app/src/main/java/com/yuan/viewpage/MainActivity.java
@@ -0,0 +1,23 @@
+package com.yuan.viewpage;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private final Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            return false;
+        }
+    });
+
+}
